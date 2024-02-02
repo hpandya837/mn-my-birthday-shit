662d424c6e7e1a2227f481eae20b85af228f6143
hpandya837 <callnirav1234@gmail.com>
2024-02-01 23:49:53 -0500

    done

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 
 //calling the 2021 function
 function n2021() {
-    document.getElementById('date').src = 'pinata.jpg';//change this
-    document.getElementById('img1').src = 'pinata.jpg';//change this
-    document.getElementById('longImage').src = 'pinata.jpg';//change this
-    document.getElementById('rightImg').src = 'pinata.jpg';//change this
-    document.getElementById('leftImg').src = 'pinata.jpg';//change this
+    document.getElementById('date').src = '2021date.png';//change this
+    document.getElementById('img1').src = '2021ins.png';//change this
+    document.getElementById('longImage').src = 'longins.jpg';//change this
+    document.getElementById('rightImg').src = 'rightins.png';//change this
+    document.getElementById('leftImg').src = 'leftins.png';//change this
     document.getElementById('textDate').innerHTML='Jan 6 2021'
     document.getElementById('textAgain').innerHTML='The Capitol Inserruction'
     document.getElementById('realText').innerHTML='The storming of the United States Capitol on January 6, 2021 was an unprecedented incident, often described as an insurrection, unfolded as supporters of then-President Donald Trump breached the Capitol building in an attempt to overturn the results of the 2020 presidential election. The riot resulted in chaos, violence, and the tragic loss of lives. The insurrection shocked the nation and prompted widespread condemnation from political leaders and citizens alike. The event led to the impeachment of President Trump for incitement of insurrection, making him the first U.S. president to be impeached twice. The January 6 insurrection also raised questions about the state of democracy and the challenges of political polarization in the United States. It remains a significant and controversial moment in recent American history.'
 }
 //calling the 1941 function
 function n1941() {
-    document.getElementById('date').src = 'pinata.jpg';//change this
-    document.getElementById('img1').src = 'pinata.jpg';//change this
-    document.getElementById('longImage').src = 'pinata.jpg';//change this
-    document.getElementById('rightImg').src = 'pinata.jpg';//change this
-    document.getElementById('leftImg').src = 'pinata.jpg';//change this
+    document.getElementById('date').src = '1941date.png';//change this
+    document.getElementById('img1').src = 'rose.png';//change this
+    document.getElementById('longImage').src = '14long.png';//change this
+    document.getElementById('rightImg').src = '14right.png';//change this
+    document.getElementById('leftImg').src = 'pointsleft.png';//change this
     document.getElementById('textDate').innerHTML='Jan 6 1941'
     document.getElementById('textAgain').innerHTML='14 Points Speech'
     document.getElementById('realText').innerHTML="In the pursuit of a better tomorrow, these 14 points serve as guiding beacons, illuminating the path toward a more united and equitable society. Embracing the principle of unity, we acknowledge that our strength lies in collective effort and collaboration. Education stands as the cornerstone, empowering individuals and propelling us towards progress. Equality becomes the driving force, ensuring that every person, irrespective of their background, is afforded the same opportunities and respect. Environmental stewardship becomes an imperative, recognizing the interconnectedness of our actions with the well-being of the planet and future generations. Through fostering innovation, we unlock the door to a future brimming with technological advancements and economic prosperity. A commitment to healthcare access reinforces the belief that well-being is a fundamental right for all. Social justice becomes the bedrock, challenging us to dismantle inequities and build a fairer society. Global cooperation extends a hand to the international community, promoting peace and understanding. Economic sustainability underscores our responsibility to balance progress with environmental and social considerations. Community engagement reinforces the strength found in togetherness, creating resilient societies. Transparency in governance builds bridges of trust between leaders and citizens. Integrating ethical considerations into technological advancements ensures that progress aligns with our moral compass. Crisis preparedness becomes our shield, fortifying us against unforeseen challenges. Ultimately, the overarching theme is one of hope – a belief that, guided by these 14 points, we can forge a future that is not only promising but also just, sustainable, and harmonious for generations to come."
 }
 //calling the 1966 function
 function n1966() {
-    document.getElementById('date').src = 'pinata.jpg';//change this
-    document.getElementById('img1').src = 'pinata.jpg';//change this
-    document.getElementById('longImage').src = 'pinata.jpg';//change this
-    document.getElementById('rightImg').src = 'pinata.jpg';//change this
-    document.getElementById('leftImg').src = 'pinata.jpg';//change this
+    document.getElementById('date').src = '1966-512.png';//change this
+    document.getElementById('img1').src = 'ar rahman-512.png';//change this
+    document.getElementById('longImage').src = '66birth.jpg';//change this
+    document.getElementById('rightImg').src = '66right-380.png';//change this
+    document.getElementById('leftImg').src = '66left-380.png';//change this
     document.getElementById('textDate').innerHTML='Jan 6 1966'
     document.getElementById('textAgain').innerHTML='A.R Rahman'
     document.getElementById('realText').innerHTML="A.R. Rahman, acclaimed as 'the Mozart of Madras,' stands as a musical luminary in the realm of Indian and global music. Born in Madras (now Chennai), India, on January 6, 1967, Rahman's journey is a testament to his unparalleled talent and groundbreaking contributions to the world of film and contemporary music. Renowned for his innovative compositions and mastery of various musical genres, Rahman has left an indelible mark on the Indian film industry and beyond. His foray into the international scene was marked by the iconic soundtrack for 'Roja'in 1992, which garnered widespread acclaim. Rahman's unique ability to blend traditional Indian sounds with modern musical elements has earned him numerous accolades, including two Academy Awards, two Grammy Awards, and a BAFTA Award. Beyond his musical prowess, Rahman is known for his humanitarian efforts and commitment to social causes. His enduring legacy continues to inspire aspiring musicians globally, solidifying his status as a musical maestro and a cultural ambassador for India on the world stage."
 }
 //calling 1993
 function n1993() {
-    document.getElementById('date').src = 'pinata.jpg';//change this
-    document.getElementById('img1').src = 'pinata.jpg';//change this
-    document.getElementById('longImage').src = 'pinata.jpg';//change this
-    document.getElementById('rightImg').src = 'pinata.jpg';//change this
-    document.getElementById('leftImg').src = 'pinata.jpg';//change this
+    document.getElementById('date').src = '1993date.png';//change this
+    document.getElementById('img1').src = 'dizzy.png';//change this
+    document.getElementById('longImage').src = 'trumpet.jpg';//change this
+    document.getElementById('rightImg').src = 'rightdizzy.png';//change this
+    document.getElementById('leftImg').src = 'leftdizzy.png';//change this
     document.getElementById('textDate').innerHTML='Jan 6 1993'
     document.getElementById('textAgain').innerHTML='Dizzy Gillespie'
     document.getElementById('realText').innerHTML="The passing of Dizzy Gillespie marked the end of an era in the world of jazz. As an American jazz trumpeter, composer, and bandleader, Gillespie was a trailblazer and a pivotal figure in the development of the bebop movement. Born on October 21, 1917, in Cheraw, South Carolina, Gillespie's impact on the jazz scene was nothing short of revolutionary. His virtuosic trumpet playing, characterized by rapid, complex runs and a distinctive bent horn, became synonymous with the bebop genre. Collaborating with other jazz luminaries such as Charlie Parker and Thelonious Monk, Gillespie's contributions elevated jazz to new heights of sophistication and improvisational brilliance. His musical innovation and charismatic stage presence left an indelible mark, influencing generations of musicians. Dizzy Gillespie's death at the age of 75 in 1993 marked the end of an extraordinary career, but his legacy lives on in the enduring influence of his groundbreaking work and his significant role in shaping the landscape of modern jazz."
 }
 function n1878() {
-    document.getElementById('date').src = 'pinata.jpg';//change this
-    document.getElementById('img1').src = 'pinata.jpg';//change this
-    document.getElementById('longImage').src = 'pinata.jpg';//change this
-    document.getElementById('rightImg').src = 'pinata.jpg';//change this
-    document.getElementById('leftImg').src = 'pinata.jpg';//change this
+    document.getElementById('date').src = '1878date.png';//change this
+    document.getElementById('img1').src = 'sandburg.png';//change this
+    document.getElementById('longImage').src = 'smokingsandburg.jpg';//change this
+    document.getElementById('rightImg').src = 'rightsandburg.png';//change this
+    document.getElementById('leftImg').src = 'leftsandburg.png';//change this
     document.getElementById('textDate').innerHTML='Jan 6 1878'
     document.getElementById('textAgain').innerHTML='Carl Sandburg'//change this
     document.getElementById('realText').innerHTML="On January 6, 1878, the world welcomed the birth of Carl Sandburg, an American poet, historian, and folklorist whose literary legacy left an indelible mark on the nation's cultural tapestry. Renowned for his eloquent prose and deep appreciation for American history, Sandburg's notable work, 'braham Lincoln: The War Years,' earned him the Pulitzer Prize for history in 1940. Born in Galesburg, Illinois, Sandburg's poetry often reflected the struggles and triumphs of everyday people, and his historical writings captured the essence of pivotal moments in American history. His exploration of Abraham Lincoln's life during the tumultuous Civil War era stands as a testament to Sandburg's commitment to preserving and understanding the nation's past. Beyond his literary achievements, Sandburg's influence extended into the realms of journalism, activism, and music. As a multifaceted figure, Carl Sandburg's contributions continue to be celebrated, reminding us of the power of words to illuminate the complexities of our shared history and inspire generations to come."
